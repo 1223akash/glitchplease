09f6a6d5c2a9137485ce0197bbafb200f5a96cef
1223akash <139630240+1223akash@users.noreply.github.com>
2023-12-14 12:04:07 +0000

    change3

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,27 +1,27 @@
-var crsr = document.querySelector("#cursor");
-var blur = document.querySelector("#cursor-blur");
+// var crsr = document.querySelector("#cursor");
+// var blur = document.querySelector("#cursor-blur");
 
-document.addEventListener("mousemove", function (dets) {
-  crsr.style.left = dets.x + "px";
-  crsr.style.top = dets.y + "px";
-  blur.style.left = dets.x - 250 + "px";
-  blur.style.top = dets.y - 250 + "px";
-  ("");
-});
+// document.addEventListener("mousemove", function (dets) {
+//   crsr.style.left = dets.x + "px";
+//   crsr.style.top = dets.y + "px";
+//   blur.style.left = dets.x - 250 + "px";
+//   blur.style.top = dets.y - 250 + "px";
+//   ("");
+// });
 
-var h4all = document.querySelectorAll("#nav h4");
-h4all.forEach(function (elem) {
-  elem.addEventListener("mouseenter", function () {
-    crsr.style.scale = 3;
-    crsr.style.border = "1px solid #fff";
-    crsr.style.backgroundColor = "transparent";
-  });
-  elem.addEventListener("mouseleave", function () {
-    crsr.style.scale = 1;
-    crsr.style.border = "1px solid #95c11e";
-    crsr.style.backgroundColor = "#95c11e";
-  });
-});
+// var h4all = document.querySelectorAll("#nav h4");
+// h4all.forEach(function (elem) {
+//   elem.addEventListener("mouseenter", function () {
+//     crsr.style.scale = 3;
+//     crsr.style.border = "1px solid #fff";
+//     crsr.style.backgroundColor = "transparent";
+//   });
+//   elem.addEventListener("mouseleave", function () {
+//     crsr.style.scale = 1;
+//     crsr.style.border = "1px solid #95c11e";
+//     crsr.style.backgroundColor = "#95c11e";
+//   });
+// });
 
 var h3all = document.querySelectorAll("#f2 h3, #f3 h3, #f4 h3");
 h3all.forEach(function (elem) {
